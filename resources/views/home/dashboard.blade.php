66ed037e96ba0828beecd8adc6413ae3fc6851f8
IconicFish12 <superglidingogre0571@gmail.com>
2022-08-04 14:40:54 +0700

    Memperbaiki Kesalahan Kecil

--- resources/views/home/dashboard.blade.php
+++ resources/views/home/dashboard.blade.php
@@ -5,7 +5,6 @@
         @if (session()->has('success'))
             <div class="alert alert-success alert-dismissible fade show py-2" role="alert">
                 {{ session()->get('success') }}
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
         @endif
         <!-- Page Heading -->
