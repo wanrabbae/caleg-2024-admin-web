45fbc3150a8f1fa9230d27cc6acfb892e8d07d2b
IconicFish12 <superglidingogre0571@gmail.com>
2022-08-04 21:22:18 +0700

    Menambahkan view info politik

--- app/Http/Controllers/InfoPolitikController.php
+++ app/Http/Controllers/InfoPolitikController.php
@@ -6,13 +6,15 @@ use Illuminate\Http\Request;
 class InfoPolitikController extends Controller
 {
     public function daftarIsuView(){
-
+        return view('data.daftarIsu');
     }
+
     public function rekapitulasiView(){
-
+        return view('data.rekapitulasi');
     }
-    public function beritaView(){
 
+    public function beritaView(){
+        return view('data.berita');
     }
 
 }
