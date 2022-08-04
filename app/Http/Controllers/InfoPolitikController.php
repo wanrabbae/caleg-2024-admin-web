e796f20cbee922b2891356365d4ff8d56898bd39
IconicFish12 <superglidingogre0571@gmail.com>
2022-08-04 20:12:12 +0700

    Menambahkan Routes Info Politik

---
+++ app/Http/Controllers/InfoPolitikController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class InfoPolitikController extends Controller
+{
+    public function daftarIsuView(){
+
+    }
+    public function rekapitulasiView(){
+
+    }
+    public function beritaView(){
+
+    }
+
+}
