ea26cb1a75b6281b17dadce8ba681afa96955989
wanrabbae <wancode.msg@gmail.com>
2022-08-05 19:45:16 +0700

    Fix minor navbar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -191,7 +191,7 @@
                     </button>
 
                     <!-- Topbar Search -->
-                    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+                    {{-- <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
                         <div class="input-group">
                             <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
                             <div class="input-group-append">
@@ -200,7 +200,7 @@
                                 </button>
                             </div>
                         </div>
-                    </form>
+                    </form> --}}
 
                     <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
@@ -364,19 +364,19 @@
 
                 </nav>
                 <!-- End of Topbar -->
-                
+
                 {{-- Session Modal --}}
                 @if (session()->has('success'))
-                <div class="alert alert-success alert-dismissible fade show py-2" role="alert">
-                    {{ session()->get('success') }}
-                </div>
+                    <div class="alert alert-success alert-dismissible fade show py-2" role="alert">
+                        {{ session()->get('success') }}
+                    </div>
                 @endif
                 @if (session()->has('error'))
-                <div class="alert alert-danger alert-dismissible fade show py-2" role="alert">
-                    {{ session()->get('error') }}
-                </div>
+                    <div class="alert alert-danger alert-dismissible fade show py-2" role="alert">
+                        {{ session()->get('error') }}
+                    </div>
                 @endif
-                
+
                 <!-- Begin Page Content -->
                 @yield('content')
                 <!-- /.container-fluid -->
