da2b1938f71eb1c70822bfef9645cdd96bc80705
IqroNegoro <iqronegoro0@gmail.com>
2022-08-05 18:45:57 +0700

    Pushing

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -376,7 +376,15 @@
                     {{ session()->get('error') }}
                 </div>
                 @endif
-                
+                @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+                    </ul>
+                </div>
+                @endif
                 <!-- Begin Page Content -->
                 @yield('content')
                 <!-- /.container-fluid -->
