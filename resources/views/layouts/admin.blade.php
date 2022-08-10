b4dbc67b0740c0f95c21cf37c367af7464a21b8e
IqroNegoro <iqronegoro0@gmail.com>
2022-08-10 20:06:07 +0700

    Fix Conflict

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -84,13 +84,8 @@
                     <div class="bg-white py-2 collapse-inner rounded">
                         <a class="collapse-item font-weight-bold" href="/relawan">Relawan</a>
                         <a class="collapse-item font-weight-bold" href="/program">Simpatisan</a>
-<<<<<<< HEAD
                         <a class="collapse-item font-weight-bold" href="/dpt">DPT</a>
-                        <a class="collapse-item font-weight-bold" href="">DPT Manual</a>
-=======
-                        <a class="collapse-item font-weight-bold" href="">DPT</a>
                         <a class="collapse-item font-weight-bold" href="/agenda">DPT Manual</a>
->>>>>>> 286c777f22e881408919e78447f3859acef99734
                         <a class="collapse-item font-weight-bold" href="">Tabulasi Suara</a>
                     </div>
                 </div>
