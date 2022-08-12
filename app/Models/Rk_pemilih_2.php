60a466714bf144df7150b16b7edb466fa8ef128f
IqroNegoro <iqronegoro0@gmail.com>
2022-08-12 08:44:56 +0700

    Fixing DPT Query

--- app/Models/Rk_pemilih_2.php
+++ app/Models/Rk_pemilih_2.php
@@ -5,21 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Rk_pemilih extends Model
+class Rk_pemilih_2 extends Model
 {
     use HasFactory;
 
     protected $table = "rk_pemilih";
 
+    protected $connection = "mysql2";
     protected $primaryKey = "id_pemilih";
 
     public $timestamps = false;
 
     protected $guarded = [];
-
-    public function desa()
-    {
-        return $this->belongsTo(Desa::class, 'id_desa');
-    }
-
 }
