6844d272dfca8483250893eb84bb68d4d3e7b82c
IqroNegoro <iqronegoro0@gmail.com>
2022-08-29 15:46:32 +0700

    Fixing Minor

--- resources/views/relawan/index.blade.php
+++ resources/views/relawan/index.blade.php
@@ -352,9 +352,10 @@
 
         function getJabatan(data) {
             fetch(`/team/${data}`).then(resp => resp.json()).then(resp => {
+                console.log(resp)
                 document.getElementById("edit_form_jabatan").action = `/team/${data}`
                 document.getElementById("edit_jabatan").value = resp.jabatan
-                document.getElementById("edit_desa").value = resp.desa
+                document.getElementById("edit_desa").value = resp.id_desa
             })
         }
     </script>    
